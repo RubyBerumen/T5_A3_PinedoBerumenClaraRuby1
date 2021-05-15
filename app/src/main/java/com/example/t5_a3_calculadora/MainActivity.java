2e7b3748ec05582a7cee602b7cb06254fc9d97f3
Ruby Berumen <clararubipb15@gmail.com>
2021-05-15 00:30:45 -0500

    Se le dió funcionalidad a los botones que no son números

--- app/src/main/java/com/example/t5_a3_calculadora/MainActivity.java
+++ app/src/main/java/com/example/t5_a3_calculadora/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     EditText caja;
+    String guardar = "";
+    String op = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +58,102 @@ public class MainActivity extends AppCompatActivity {
 
     public void btn9 (View v){
         caja.setText(caja.getText().toString() + "9");
+    }
+
+    public void btnResiduo (View v){
+        guardar = caja.getText().toString();
+        caja.setText("");
+        op = "%";
+    }
+
+    public void btnRaiz (View v){
+        if(caja.getText().length()>0) {
+            double num = Double.parseDouble(caja.getText().toString());
+            if(num>0) {
+                num = Math.sqrt(num);
+                caja.setText(Double.toString(num));
+            }
+        }
+    }
+
+    public void btnPotencia (View v){
+        if(caja.getText().length()>0) {
+            double num = Double.parseDouble(caja.getText().toString());
+            num = Math.pow(num, 2);
+            caja.setText(Double.toString(num));
+        }
+    }
+
+    public void btnLn (View v){
+        if(caja.getText().length()>0) {
+            double num = Double.parseDouble(caja.getText().toString());
+            if(num!=0) {
+                num=1/num;
+                caja.setText(Double.toString(num));
+            }
+        }
+    }
+
+    public void btnBorrarT (View v){
+        guardar = "";
+        caja.setText("");
+        op = " ";
+    }
+
+    public void btnBorrar (View v){
+        if(caja.getText().length()>0)
+            caja.setText(caja.getText().toString().substring(0,caja.getText().length()-1));
+    }
+
+    public void btnDiv (View v){
+        guardar = caja.getText().toString();
+        caja.setText("");
+        op = "/";
+    }
+
+    public void btnMul (View v){
+        guardar = caja.getText().toString();
+        caja.setText("");
+        op = "*";
+    }
+
+    public void btnRest (View v){
+        guardar = caja.getText().toString();
+        caja.setText("");
+        op = "-";
+    }
+
+    public void btnSum (View v){
+        guardar = caja.getText().toString();
+        caja.setText("");
+        op = "+";
+    }
+
+    public void btnPunto (View v){
+        if(!caja.getText().toString().contains("."))
+            caja.setText(caja.getText()+".");
+    }
+
+    public void btnIgual (View v){
+        if(op!=" ") {
+            double n1 = Double.parseDouble(guardar);
+            double n2 = Double.parseDouble(caja.getText().toString());
+            double res = 0;
+            switch (op) {
+                case "%" :
+                    res = n1%n2;break;
+                case "/" :
+                    res = n1/n2;break;
+                case "*" :
+                    res = n1*n2;break;
+                case "-" :
+                    res = n1-n2;break;
+                case "+" :
+                    res = n1+n2;break;
+                default : break;
+            }
+            caja.setText(Double.toString(res));
+        }
     }
 
 
